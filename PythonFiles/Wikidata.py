6b478dd1ec452252ce33a66bf32da0f989cc0b32
Keno-git <61561186+Keno-git@users.noreply.github.com>
2022-12-09 22:45:22 +0100

    Director and publication year

--- PythonFiles/Wikidata.py
+++ PythonFiles/Wikidata.py
@@ -10,23 +10,40 @@ def printjson(result):
     for item in bindings:
         print(item)
 
-movie = "Interstellar" #able to change movie here
-sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
-sparql.setReturnFormat(JSON)
-queryString = """SELECT ?Movie ?nameOfRole ?name WHERE {
-  ?movie rdfs:label '"""+ movie +"""'@en;
-         p:P161 ?member;
-         p:P1476 [ps:P1476 ?Movie].
+def getActorsAndRole(movie = "Interstellar"):
+  sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
+  sparql.setReturnFormat(JSON)
+  queryString = """SELECT ?Movie ?nameOfRole ?name WHERE {
+    ?movie rdfs:label '"""+ movie +"""'@en;
+          p:P161 ?member;
+          p:P1476 [ps:P1476 ?Movie].
+    
+    ?member pq:P453 [rdfs:label ?nameOfRole];
+            ps:P161 [rdfs:label ?name].
+
+    filter(lang(?name)="en")
+    filter(lang(?nameOfRole)="en")
+  }limit 200"""
+
+  sparql.setQuery(queryString)
+  result = sparql.queryAndConvert()
+  printjson(result)
+
+def getDirectorandYear(movie = "Interstellar"):
+  sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
+  sparql.setReturnFormat(JSON)
+  queryString = """SELECT ?date ?director WHERE {
+  ?movie rdfs:label '""" + movie + """'@en;
+         p:P577 [ps:P577 ?date];
+         p:P57 [ps:P57 [rdfs:label ?director]].
+  filter(lang(?director)='en')
+}"""
+
+  sparql.setQuery(queryString)
+  result = sparql.queryAndConvert()
+  printjson(result)
   
-  ?member pq:P453 [rdfs:label ?nameOfRole];
-          ps:P161 [rdfs:label ?name].
+  print()
 
-  filter(lang(?name)="en")
-  filter(lang(?nameOfRole)="en")
-}limit 200"""
-
-sparql.setQuery(queryString)
-result = sparql.queryAndConvert()
-printjson(result)
-
-
+getActorsAndRole()
+getDirectorandYear()

38d858610ce6da236ca625a1d282cb8c28a4793d
Keno-git <61561186+Keno-git@users.noreply.github.com>
2022-12-16 20:41:02 +0100

    Ui some user error handling

--- PythonFiles/UserInterface.py
+++ PythonFiles/UserInterface.py
@@ -1,6 +1,17 @@
 import MovieActorInformation
+def capitilize(input):
+    splited = input.split(" ")
+    tmp = ""
+    for stringPart in splited:
+        tmp += stringPart.capitalize() + " "
+    return tmp.strip()
+
+
 
 def __main__():
+    print("************************\n****IMPORTANT NOTICE****\n************************")
+    print("Currently there is no major User input error handling, Make sure to spell the name of the actor or movie correct.\n")
+    print("************************\n****IMPORTANT NOTICE****\n************************")
     print("Welcome")
     print("To generate questions about Actors enter 1 for Movies enter 2 to exit enter 3.\n")
     
@@ -8,11 +19,11 @@ def __main__():
     while("3" not in choice):
         if("1" in choice):
             print("Enter an Actor to get questions about.")
-            actor = input()
+            actor = capitilize(input())
             json = MovieActorInformation.createQuizActor(actor)
         else:
             print("Enter an Movie to get questions about.")
-            movie = input()
+            movie = capitilize(input())
             json = MovieActorInformation.createQuizMovie(movie)
         print(json)
 
